f1c2c622448dc8ceead16163f6d5ebf96f6dee35
atiqureee51 <60899605+atiqureee51@users.noreply.github.com>
2023-02-05 03:37:52 -0600

    Update maintracker.py

--- maintracker.py
+++ maintracker.py
@@ -52,7 +52,7 @@ st.set_page_config(page_title='Road accident tracker in Bangladesh')
 [![Follow](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/tracknews)
 [![Follow](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/tracknews)
 
-# Road accident tracker bd Road Accident MAP Bangladesh
+# Road Accident MAP Bangladesh
 """
 # Add a sidebar to the web page. 
 st.markdown('---')
